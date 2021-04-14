b070e96bd72c58f70a4806de7b17cd41d008e3f8
crypt0s0nic <sondnm.blockchain@gmail.com>
2021-04-14 20:01:39 +0700

    Add Airdrop contract

---
+++ contracts/Airdrop.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.7.4;
+
+import "@openzeppelin/contracts/utils/Context.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract Airdrop is Context, Ownable {
+    using SafeMath for uint256;
+
+    IERC20 public token;
+
+    constructor(address _token) public {
+        token = IERC20(_token);
+    }
+
+    function sendBatch(address[] calldata recipients, uint256[] calldata recipientsBalance, uint256 totalAmount) external onlyOwner {
+        require(recipients.length == recipientsBalance.length, "Airdrop::sendBatch: unbalanced recipients data");
+        require(totalAmount > 0, "Airdrop::sendBatch: totalAmount must be positive");
+        uint256 totalShare = 0;
+        for (uint256 i = 0; i < recipientsBalance.length; i++) {
+            totalShare = totalShare.add(recipientsBalance[i]);
+        }
+        uint256 totalAmountLeft = totalAmount;
+        uint256 airdropAmount;
+
+        for (uint256 i = 0; i < recipients.length; i++) {
+            airdropAmount = totalAmount.mul(recipientsBalance[i]).div(totalShare);
+            if (i == (recipients.length - 1)) {
+                airdropAmount = totalAmountLeft;
+            }
+            if (airdropAmount > 0) {
+                token.transfer(recipients[i], airdropAmount);
+                totalAmountLeft = totalAmountLeft.sub(airdropAmount);
+            }
+        }
+    }
+}
