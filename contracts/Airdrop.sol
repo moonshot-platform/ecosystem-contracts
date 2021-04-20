06653180d595d04488a2b95db7a181bbdd1dfc09
crypt0s0nic <sondnm.blockchain@gmail.com>
2021-04-21 00:46:19 +0700

    Remove unnecessary line of code

--- contracts/Airdrop.sol
+++ contracts/Airdrop.sol
@@ -7,8 +7,6 @@ import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-import "hardhat/console.sol";
-
 contract Airdrop is Context, Ownable {
     using SafeMath for uint256;
 
